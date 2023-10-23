c4edb00789504d4d707d15334906b87456ce200e
Yusuke Matsui <matsui528@gmail.com>
2023-10-23 16:20:06 +0900

    flatten dataset

--- download_caltech256.py
+++ download_caltech256.py
@@ -1,4 +1,6 @@
 import torchvision
+import pathlib
+
 _ = torchvision.datasets.Caltech256(root='./img', download=True)
 
 # The images will be downloaded in ./img, i.e., 
@@ -14,3 +16,9 @@ _ = torchvision.datasets.Caltech256(root='./img', download=True)
 #        ...
 #      ...
 
+# Then, rename them, e.g., 
+# ./img/caltech256/001.ak47/001_0001.jpg -> ./img/001_0001.jpg
+root_dir = pathlib.Path('./img/caltech256')
+for file_path in root_dir.glob('**/*.jpg'):
+    new_path = pathlib.Path('./img') / file_path.name
+    file_path.rename(new_path)
